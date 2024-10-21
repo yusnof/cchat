010813b5a0c54ebbbcf15a0257b097bd5d143c3c
yusnof <yusnofal@gmail.com>
2024-10-21 14:00:39 +0200

    we added comments, and handed in
    Co-authored-by: rikypara04 <rikypara04@users.noreply.github.com>

--- channel.erl
+++ channel.erl
@@ -1,17 +1,40 @@
 -module(channel).
 -export([start/1,stop/1]).
 
+% Record definition for the channel state.
+% Fields:
+%   - name: The name of the channel.
+%   - members: A list of PIDs representing the members of the channel.
 -record(channel_st, {
     name,   
     members 
 }).
 
+
+
+start(Name) ->
+    genserver:start(list_to_atom(Name), initial_state(Name), fun handle/2).
+
+% Stop a channel process by its name.
+% Converts the channel name to an atom and stops the genserver.
+stop(Name) ->
+    genserver:stop(list_to_atom(Name)).
+
+
+% Initialize the channel state with a given name.
+% Arguments:
+%   - Name: The name of the channel.
+% Returns a new channel state record with an empty member list.
 initial_state(Name) ->
     #channel_st{
         name = Name,
         members = []
     }.
-% Client request to join
+% Handle client request to join the channel.
+% Arguments:
+%   - St: Current channel state.
+%   - {join, Pid}: A tuple indicating a join request, where Pid is the client's process ID.
+% If the client is already a member, returns an error. Otherwise, adds the client to the member list.
 handle(St, {join, Pid}) ->
     case lists:member(Pid, St#channel_st.members) of
         true ->
@@ -21,7 +44,11 @@ handle(St, {join, Pid}) ->
             {reply, ok, St#channel_st{members=TempList}}
         end;
 
-% Client request to leave
+% Handle client request to leave the channel.
+% Arguments:
+%   - St: Current channel state.
+%   - {leave, Pid}: A tuple indicating a leave request, where Pid is the client's process ID.
+% If the client is a member, they are removed from the list. If not, returns an error.
 handle(St, {leave, Pid}) ->
     case lists:member(Pid, St#channel_st.members) of
         true ->
@@ -31,6 +58,14 @@ handle(St, {leave, Pid}) ->
             {reply, {error, not_a_member, "Client not a member of " ++ St#channel_st.name}, St}
     end; 
 
+% Handle message send request from a client.
+% Arguments:
+%   - St: Current channel state.
+%   - {message_send, ClientPid, ClientNick, Msg}: A tuple representing the message send request.
+%     ClientPid: The PID of the client sending the message.
+%     ClientNick: The nickname of the client.
+%     Msg: The message content.
+% The message is sent to all other members of the channel except the sender.
 handle(St, {message_send, ClientPid, ClientNick, Msg}) ->
     OtherMembers = lists:delete(ClientPid, St#channel_st.members),
     Data = {request, self(), make_ref(), {message_receive, St#channel_st.name, ClientNick, Msg}},
@@ -40,9 +75,3 @@ handle(St, {message_send, ClientPid, ClientNick, Msg}) ->
 handle(St, _) ->
     {reply, {error, not_implemented, "Channel cannot handle this request!"}, St} .
 
-
-start(Name) ->
-    genserver:start(list_to_atom(Name), initial_state(Name), fun handle/2).
-
-stop(Name) ->
-    genserver:stop(list_to_atom(Name)).
--- server.erl
+++ server.erl
@@ -2,10 +2,11 @@
 -export([start/1, stop/1]).
 
 -record(serverState, {
-    users,
-    channels
+    users,       % List of users registered on the server
+    channels     % List of channels available on the server
 }).
 
+% Initializes the server state with empty user and channel lists
 initial_state() ->
     #serverState{
         users = [],
@@ -13,74 +14,77 @@ initial_state() ->
     }.
 
 % Start a new server process with the given name
-% Do not change the signature of this function.
 start(ServerAtom) ->
     catch genserver:start(ServerAtom, initial_state(), fun handle/2).
 
-% Stop the server process registered to the given name,
-% together with any other associated processes
+% Stop the server process registered to the given name with any other associated processes (when we stop server)
 stop(ServerAtom) ->
-    genserver:request(ServerAtom, delete_all_channels),
-    genserver:stop(ServerAtom).
+    genserver:request(ServerAtom, delete_all_channels),  % Request to delete all channels
+    genserver:stop(ServerAtom).                          % Stop the server process
 
+% Handle the user joining a channel
 handle(State, {join, Pid, NewUserName, Channel}) ->
+        % Update the user list if the new username isn't already present
         NewNicksList = case lists:member(NewUserName, State#serverState.users) of
-            true -> State#serverState.users;
-            false -> [NewUserName | State#serverState.users]
+            true -> State#serverState.users;  % Keep the list unchanged
+            false -> [NewUserName | State#serverState.users]  % Add new username
         end,
+        % Add the channel to the list if it's not already present
         NewChannelsList = case lists:member(Channel, State#serverState.channels) of
-            true -> State#serverState.channels;
+            true -> State#serverState.channels;  % Keep the list unchanged
             false -> 
-                channel:start(Channel), 
-                [Channel | State#serverState.channels]
+                channel:start(Channel),  % Start the new channel
+                [Channel | State#serverState.channels]  % Add the new channel
         end,
+        % Try sending a join request to the channel
         try 
             ChannelResponse = genserver:request(list_to_atom(Channel), {join, Pid}),
             {reply, ChannelResponse, State#serverState{users= NewNicksList, channels=NewChannelsList}}
         catch
-            _:_Error -> {reply, {error, join_failed, "Failed to join channel " ++ Channel}, State}
+            _:_Error -> {reply, {error, join_failed, "Failed to join channel " ++ Channel}, State}  % Handle errors
         end;    
 
-
+% Handle the user quitting the server
 handle(State, {quit, UserName}) ->
-    TempList = lists:delete(UserName, State#serverState.users),
-    {reply, ok, State#serverState{users = TempList}}; 
+    TempList = lists:delete(UserName, State#serverState.users),  % Remove the user from the list
+    {reply, ok, State#serverState{users = TempList}};  % Reply with the updated state
 
-%function that handles changing NickName 
-handle(State,{nick, OldUserName, NewUserName})-> 
+% Handle changing a user's nickname
+handle(State, {nick, OldUserName, NewUserName}) -> 
     case lists:member(NewUserName, State#serverState.users) of
-        true when OldUserName =:= NewUserName ->
-            {reply, ok, State};
-        true ->
-            io:format("Hello, world!~n"),
-            {reply, {error, nick_taken, "Username "++NewUserName++" has been taken!"}, State};
-        false ->
-            TempList = [NewUserName | lists:delete(OldUserName, State#serverState.users)],
-            {reply, ok, State#serverState{users=TempList}}
-        end;
+        true when OldUserName =:= NewUserName ->  % No change in nickname
+            {reply, ok, State};  % Reply with the current state
+        true ->  % New username already taken
+            io:format("Hello, world!~n"),  % Debugging message
+            {reply, {error, nick_taken, "Username " ++ NewUserName ++ " has been taken!"}, State};  % Error reply
+        false ->  % New username is available
+            TempList = [NewUserName | lists:delete(OldUserName, State#serverState.users)],  % Update user list
+            {reply, ok, State#serverState{users = TempList}}  % Reply with the updated state
+    end;
 
-
+% Handle the delete of all channels
 handle(State, delete_all_channels) ->
-lists:foreach(
-fun(Ch) -> genserver:stop(list_to_atom(Ch)) end, State#serverState.channels
-),
+    lists:foreach(
+        fun(Ch) -> genserver:stop(list_to_atom(Ch)) end,  % Stop each channel process
+        State#serverState.channels
+    ),
+    {reply, ok, State#serverState{users = [], channels = []}};  % Clear the state and reply
 
-{reply,ok, State#serverState{users=[], channels = []}};
-
+% Handle sending a message to a channel
 handle(State, {message_send, Channel, Msg}) ->
-    ChannelAtom = list_to_atom(Channel),
+    ChannelAtom = list_to_atom(Channel),  % Convert channel name to atom to be able to check if it exists or not
     case lists:member(Channel, State#serverState.channels) of
-        true ->
+        true ->  % Channel exists
             try
-                ChannelResponse = genserver:request(ChannelAtom, {message_send, self(), Msg}),
-                {reply, ChannelResponse, State}
+                ChannelResponse = genserver:request(ChannelAtom, {message_send, self(), Msg}),  % Send the message
+                {reply, ChannelResponse, State}  % Reply with the response
             catch
-                _:_Error -> {reply, {error, server_not_reached, "Failed to send message to channel " ++ Channel}, State}
+                _:_Error -> {reply, {error, server_not_reached, "Failed to send message to channel " ++ Channel}, State}  % Handle errors
             end;
-        false ->
-            {reply, {error, channel_not_exists, "Channel " ++ Channel ++ " does not exist"}, State}
-        end; 
+        false ->  % Channel does not exist
+            {reply, {error, channel_not_exists, "Channel " ++ Channel ++ " does not exist"}, State}  % Error reply
+    end;
 
-%ToDo catch other commands with some type of exeption
-handle(State, _ ) ->
-    {reply, {error, not_implemented}, State}.
+% Catch-all handler for unimplemented commands
+handle(State, _) ->
+    {reply, {error, not_implemented}, State}.  % Reply with an error message
